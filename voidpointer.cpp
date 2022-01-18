9ff3c5566e3da9365f9b55e1daba47d53bb34ab2
ArshnoorKaur21 <97722920+ArshnoorKaur21@users.noreply.github.com>
2022-01-18 22:24:03 +0530

    void-pointer

--- voidpointer.cpp
+++ voidpointer.cpp
@@ -5,6 +5,6 @@ int main()
 	float b=24.5;
 	void *ptr;
 	ptr=&a;
-	printf("the value of a is %d\n",*( (int*)ptr));//here we cant derefrence void pointer chahe a ka address bhu daldo
+	printf("the value of a is %d\n",*( (int*)ptr));//here we cant derefrence void pointer even if we put address of a
 	
 }
