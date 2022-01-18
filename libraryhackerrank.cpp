f5f4b391b6060e942775485e404b2a3d615d8720
ArshnoorKaur21 <97722920+ArshnoorKaur21@users.noreply.github.com>
2022-01-19 00:02:07 +0530

    library(hackerrank)

---
+++ libraryhackerrank.cpp
@@ -0,0 +1,16 @@
+//printing the digit frequency in single line
+#include<stdio.h>
+#include<string.h>
+main()
+{
+	int n,i;
+	printf("\nenter the ten space seperate integers in single line");
+	scanf("%d",&n);
+    for(i=0;i<strlen(n);i++)
+    {
+    	printf("\n",n);
+	}
+    return 0;
+	
+	
+}
