7285c8fe6013cf531f783b7a8c7bdefc92afdf09
ArshnoorKaur21 <97722920+ArshnoorKaur21@users.noreply.github.com>
2022-01-18 23:19:26 +0530

    square-root

---
+++ squarreroot.cpp
@@ -0,0 +1,13 @@
+//program ot find square root and power of the function 
+#include<stdio.h>
+#include<math.h>
+main()
+{
+	int n;
+	printf("square root of a number");
+	scanf("%d",&n);
+    int ans=sqrt(n);
+	printf("square root of %d=%d",n,ans);
+	return 0;
+}
+
