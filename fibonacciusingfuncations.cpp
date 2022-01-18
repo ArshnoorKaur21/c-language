73f8e6b44f160bd0fe567507487be78cb2b6f95a
ArshnoorKaur21 <97722920+ArshnoorKaur21@users.noreply.github.com>
2022-01-19 00:14:34 +0530

    fibo(using-fxns)

---
+++ fibonacciusingfuncations.cpp
@@ -0,0 +1,25 @@
+//program of febonacci series
+#include<stdio.h>
+void fibonacci()
+{
+	int i,c,n;
+	for(i=0;i<=n;i++);
+	{
+		c=a+b;
+		{
+			printf("%d",c);
+			a=b;
+			b=c;
+		}
+	}
+}
+main()
+{
+	int a=0,b=1;
+	int n;
+	printf("enter the fibonacci series\n");
+	printf("enter the limit\n");
+	scanf("%d",&n);
+	fibonacci(a,b);
+	return 0;
+}
