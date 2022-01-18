7e4bee0196126bb6df94484dbf4f5c30d8bd00f7
ArshnoorKaur21 <97722920+ArshnoorKaur21@users.noreply.github.com>
2022-01-19 00:04:10 +0530

    leet-code-prblms

---
+++ leetcodeprblms.cpp
@@ -0,0 +1,20 @@
+//program of median of two sorted arrays
+#include<stdio.h>
+main()
+{
+   int a[5];
+   int b[5];
+   printf("\nfirst array");
+   for(i=0;i<5;i++)
+   {
+   	scanf("%d",&a[i]);
+   }
+   printf("\nsecond array");
+   for(i=0;i<5;i++)
+   {
+   	scanf("%d",&b[i]);
+   }
+   printf("\nmerged array of 1sta nd second");
+   
+   
+}
