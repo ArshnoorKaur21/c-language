8f19111f17f3bbcc5089c5a4db1034512cf9f22a
ArshnoorKaur21 <97722920+ArshnoorKaur21@users.noreply.github.com>
2022-01-19 00:10:04 +0530

    if-else

---
+++ ifelse.cpp
@@ -0,0 +1,14 @@
+//program
+#include<stdio.h>
+main()
+{
+	int a,b;
+	printf("greater among two numbers:");
+	scanf("%d%d",&a,&b);
+		if(a>b)
+		printf("%d is greater",a);
+		else
+		printf("%d is greater",b);
+	
+	return 0;
+}
