58434a4a44617e2c8f119b00144cd70477e2a36f
ArshnoorKaur21 <97722920+ArshnoorKaur21@users.noreply.github.com>
2022-01-18 23:18:30 +0530

    star-pattern

---
+++ starpattern.cpp
@@ -0,0 +1,16 @@
+//program
+#include<stdio.h>
+main()
+{
+	int i,j;
+	printf("\nstar pattern \n");
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("*");
+		}
+	printf("\n");
+	}
+	return 0;
+}
