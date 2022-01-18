61e4eb8f5ee842c101d70fe6cd4b7139c20bc946
ArshnoorKaur21 <97722920+ArshnoorKaur21@users.noreply.github.com>
2022-01-18 23:46:07 +0530

    pointers-in-funcitons

---
+++ pointersinfxns.cpp
@@ -0,0 +1,24 @@
+//program of pointer to call by value
+#include<stdio.h>
+void swap(int a,int b)
+{
+ int c;
+ c=a;
+ a=b;
+ b=c;
+ printf("\ninside the function");
+ printf("\n a=%d \t b=%d",a,b);
+ 	
+}
+main()
+{
+	int a,b;
+	printf("\nenter two numbers");
+	scanf("%d%d",&a,&b);
+	printf("\nbefore calling the function");
+	printf("\n a=%d b=%d",a, b);
+	swap(a,b);
+	printf("\nafter calling the function");
+	printf("\n a=%d b=%d",a, b);
+	
+}
