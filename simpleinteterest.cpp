33f4683611a5f1a378b9167aa470996c7348b386
ArshnoorKaur21 <97722920+ArshnoorKaur21@users.noreply.github.com>
2022-01-18 23:21:30 +0530

    simple-interest

---
+++ simpleinteterest.cpp
@@ -0,0 +1,17 @@
+//program of simple interest
+#include<stdio.h>
+void simpleinterst(float p,float r,float t)
+{
+	float si;
+	printf("\nsimple interest");
+	si=(p*r*t)/100;
+	printf(" %f",si);
+}
+main()
+{
+	float p,r,t;
+	printf("enter principal,rate,time");
+	scanf("%f%f%f",&p,&r,&t);
+	simpleinterst(p,r,t);
+	return 0;
+}
