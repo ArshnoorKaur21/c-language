72c5160803c4eb1f8c58ffb49b8d681f914a3426
ArshnoorKaur21 <97722920+ArshnoorKaur21@users.noreply.github.com>
2022-01-19 00:13:11 +0530

    fibonacci(recursion)

---
+++ fibosuingrecursion.cpp
@@ -0,0 +1,19 @@
+//fibo using recursion
+#include<stdio.h>
+int fibo(int n){
+	if(n==1){
+		return 0;
+	}
+	else if(n==2){
+		return 1;
+	}
+	else{
+		return fibo(n-1)+fibo(n-2);
+	}
+}
+int main(){
+	int n;
+	scanf("%d",&n);
+	int a=fibo(n);
+	printf("%d",a);
+}
