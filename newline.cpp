fced2f0ebbd185b0ac522a95d404478ceb6eb599
ArshnoorKaur21 <97722920+ArshnoorKaur21@users.noreply.github.com>
2022-01-18 23:53:04 +0530

    newline

---
+++ newline.cpp
@@ -0,0 +1,14 @@
+//program of pritning tokens
+#include<stdio.h>
+main()
+{
+	int i;
+	char sen[20];
+	printf("\nenter the sentence you want to print");
+    gets(sen);
+	for(i=0;i<22;i++)
+	{
+	    puts("\n");
+	}
+	
+}
