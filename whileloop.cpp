75fa12c412ca632c334fc040e6810f8d6ce64c8d
ArshnoorKaur21 <97722920+ArshnoorKaur21@users.noreply.github.com>
2022-01-18 22:20:49 +0530

    whileloop

---
+++ whileloop.cpp
@@ -0,0 +1,22 @@
+//practice of while and do while loop
+#include<stdio.h>
+main()
+{
+	int i;
+	printf("\n While Loop Practice \n");
+	i=11;
+	while(i<=10)
+	{
+		printf("\n %d",i);
+		i++;
+	}
+	//do while loop
+		printf("\n Do While Loop Practice \n");
+	i=1;
+	do
+	{
+		printf("\n %d",i);
+		i++;
+	}while(i<=10);
+	return 0;
+}
