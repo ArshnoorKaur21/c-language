b4c53ad7f75192818ba801209cb9188d1d2521c0
ArshnoorKaur21 <97722920+ArshnoorKaur21@users.noreply.github.com>
2022-01-19 00:01:04 +0530

    linear-search

---
+++ linearsearch.cpp
@@ -0,0 +1,23 @@
+//program 
+#include<stdio.h>
+main()
+{
+	int loc=-1;
+	int i,n,item;
+	printf("enter size of array");
+	scanf("%d",&n);
+	int
+	
+	printf("linear searching");
+	for(i=0;i<n;i++)
+	{
+		if(a[i]==item)
+		{
+			loc=i;
+			printf("element found at the location";)
+		}
+	}
+	if(i==n)
+	printf("elemetn not found at location");
+	return 0;
+}
